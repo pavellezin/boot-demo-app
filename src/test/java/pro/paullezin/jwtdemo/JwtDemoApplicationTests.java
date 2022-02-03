05efe7fff1a98672afd3912ca2a626c0c51d2889
pavel.lezin <pavellezin@gmail.com>
2022-02-03 16:45:03 +0500

    init

---
+++ src/test/java/pro/paullezin/jwtdemo/JwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.paullezin.jwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
