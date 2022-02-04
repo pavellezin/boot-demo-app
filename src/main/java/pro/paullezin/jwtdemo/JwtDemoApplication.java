1671517f45224f26b12bf0543130d8b40ec62277
pavel.lezin <pavellezin@gmail.com>
2022-02-04 15:19:02 +0500

    add liquibase, init ddl

--- src/main/java/pro/paullezin/jwtdemo/JwtDemoApplication.java
+++ src/main/java/pro/paullezin/jwtdemo/JwtDemoApplication.java
@@ -1,14 +1,7 @@
 package pro.paullezin.jwtdemo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import pro.paullezin.jwtdemo.model.Role;
-import pro.paullezin.jwtdemo.model.User;
-import pro.paullezin.jwtdemo.service.UserService;
-
-import java.util.EnumSet;
 
 @SpringBootApplication
 public class JwtDemoApplication {
@@ -17,12 +10,4 @@ public class JwtDemoApplication {
         SpringApplication.run(JwtDemoApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner run(UserService userService) {
-        return args -> {
-            userService.saveUser(new User("John Travolta", "john", "1234", EnumSet.of(Role.ADMIN, Role.USER)));
-            userService.saveUser(new User("Will Smith", "will", "12345", EnumSet.of(Role.ADMIN, Role.USER)));
-            userService.saveUser(new User("Bruce Willis", "bruce", "123456", EnumSet.of(Role.USER)));
-        };
-    }
 }
