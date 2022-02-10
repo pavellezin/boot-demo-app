6114dd2328430daba7bbbab4588d21661a8fa03d
pavel.lezin <pavellezin@gmail.com>
2022-02-10 11:57:30 +0500

    add docker

--- src/main/java/pro/paullezin/jwtdemo/model/BaseEntity.java
+++ src/main/java/pro/paullezin/jwtdemo/model/BaseEntity.java
@@ -17,7 +17,7 @@ import javax.persistence.*;
 public class BaseEntity implements Persistable<Integer> {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Override
